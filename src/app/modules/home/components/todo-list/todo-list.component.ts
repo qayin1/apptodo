23c036975a1af7ef8447412916ef1e4f6de4cd09
qayin1 <jumpgash@hotmail.com>
2023-04-12 15:37:05 -0300

    first commit

---
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, NgModule, DoCheck   } from '@angular/core';
+import { TaskList } from '../../model/task-list';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements DoCheck{
+ public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[]');
+ constructor() {}
+
+ ngDoCheck(){
+  this.setLocalStorage()
+ }
+
+ public deleteItemTaskList(event: number){
+  this.taskList.splice(event, 1)
+ }
+
+ public setEmitTaskList(event: string) {
+  this.taskList.push({task: event, checked: false})
+ }
+
+ public deleteAllTaskList() {
+  const confirm = window.confirm('Você deseja realmente deletar tudo?')
+  if(confirm){
+
+    this.taskList = []
+  }
+ }
+
+ public validationInput(event: string, index:number) {
+  if(!event.length) {
+    const confirm = window.confirm("Task está vazia, deseja deletar?")
+    if(confirm) {
+      this.deleteItemTaskList(index)
+    }
+  }
+ }
+
+ public setLocalStorage() {
+  if(this.taskList) {
+    this.taskList.sort( (first, last) => Number(first.checked) - Number(last.checked))
+    localStorage.setItem("list", JSON.stringify(this.taskList))
+  }
+ }
+}
